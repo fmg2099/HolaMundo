04331ac5cdd74b0f4c71c2ce75916bbba8bde8e9
Alumno <Alumno@01WS-LAB501-07>
2023-01-11 15:07:44 -0600

    Add project files.

---
+++ HolaMundo/HolaMundo.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h> //biblioteca de C
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+	//crea un apuntador de chars
+	char* palabra;
+	//pide 64 bytes de memoria
+	palabra = (char*)malloc(64);
+	//limpiar el contenido del apuntador
+	memset(palabra, 0, 64);
+
+	printf( palabra  );
+	return 0;
+}
